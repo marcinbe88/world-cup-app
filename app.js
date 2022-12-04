e69b65e8d8053bf0c0a41773223d8b72cf702723
Marcin Bejger <marcin.bejger@gmail.com>
2022-12-04 15:00:25 +0100

    dynamic team names added

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log('dziala')
+const submitButton = document.getElementById("submit-btn")
+const team1Input = document.getElementById("team-1-input")
+const team2Input = document.getElementById("team-2-input")
+const team3Input = document.getElementById("team-3-input")
+const team4Input = document.getElementById("team-4-input")
+
+const roundsTeam1Name = document.getElementById("team-1-name")
+const roundsTeam2Name = document.getElementById("team-2-name")
+const roundsTeam3Name = document.getElementById("team-3-name")
+const roundsTeam4Name = document.getElementById("team-4-name")
+
+submitButton.addEventListener("click", confirmTeams)
+
+function confirmTeams() {
+    console.log('teams are updated')
+    const groupTeamsList = document.getElementById("group-teams")
+
+    const team1Name = document.createElement("p")
+    const team2Name = document.createElement("p")
+    const team3Name = document.createElement("p")
+    const team4Name = document.createElement("p")
+
+    team1Name.innerText = `${team1Input.value}`
+    team2Name.innerText = `${team2Input.value}`
+    team3Name.innerText = `${team3Input.value}`
+    team4Name.innerText = `${team4Input.value}`
+
+    roundsTeam1Name.innerText = `${team1Input.value}`
+    roundsTeam2Name.innerText = `${team2Input.value}`
+    roundsTeam3Name.innerText = `${team3Input.value}`
+    roundsTeam4Name.innerText = `${team4Input.value}`
+
+    groupTeamsList.append(team1Name,team2Name,team3Name,team4Name)
+}
