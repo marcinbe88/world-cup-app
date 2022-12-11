3246f2cf0bcb9bba44c902e4247e88f75d21a05b
Marcin Bejger <marcin.bejger@gmail.com>
2022-12-11 13:34:13 +0100

    3 rounds team problem fixed

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const roundsTeam4Name = document.getElementsByClassName("team-4-name")
 submitButton.addEventListener("click", confirmTeams)
 
 function confirmTeams() {
-    console.log('teams are updated')
     const groupTeamsList = document.getElementById("group-teams")
 
     const team1Name = document.createElement("p")
@@ -30,10 +29,61 @@ function confirmTeams() {
     // roundsTeam3Name.innerText = team3Name
     // roundsTeam4Name.innerText = team4Name
 
-    roundsTeam1Name.innerText = `${team1Input.value}`
-    roundsTeam2Name.innerText = `${team2Input.value}`
-    roundsTeam3Name.innerText = `${team3Input.value}`
-    roundsTeam4Name.innerText = `${team4Input.value}`
+    const round1Div = document.getElementById("round-1-div")
+    const round2Div = document.getElementById("round-2-div")
+    const round3Div = document.getElementById("round-3-div")
+
+    const round1 = document.getElementById("round-1")
+    const round2 = document.getElementById("round-2")
+    const round3 = document.getElementById("round-3")
+
+    round1Div.innerHTML = `
+    <div>
+    <p>${team1Input.value}</p>
+    <input placeholder="goals" type="text" class="team-1-goals" />
+    <input placeholder="goals" type="text" class="team-2-goals" />
+    <p>${team2Input.value}</p>
+    </div>
+    <div>
+    <p>${team3Input.value}</p>
+    <input placeholder="goals" type="text" class="team-3-goals" />
+    <input placeholder="goals" type="text" class="team-4-goals" />
+    <p>${team4Input.value}</p>
+    </div>
+    `
+
+    round2Div.innerHTML = `
+    <div>
+    <p>${team1Input.value}</p>
+    <input placeholder="goals" type="text" class="team-1-goals" />
+    <input placeholder="goals" type="text" class="team-2-goals" />
+    <p>${team3Input.value}</p>
+    </div>
+    <div>
+    <p>${team2Input.value}</p>
+    <input placeholder="goals" type="text" class="team-3-goals" />
+    <input placeholder="goals" type="text" class="team-4-goals" />
+    <p>${team4Input.value}</p>
+    </div>
+    `
+
+    round3Div.innerHTML = `
+    <div>
+    <p>${team1Input.value}</p>
+    <input placeholder="goals" type="text" class="team-1-goals" />
+    <input placeholder="goals" type="text" class="team-2-goals" />
+    <p>${team4Input.value}</p>
+    </div>
+    <div>
+    <p>${team3Input.value}</p>
+    <input placeholder="goals" type="text" class="team-3-goals" />
+    <input placeholder="goals" type="text" class="team-4-goals" />
+    <p>${team2Input.value}</p>
+    </div>
+    `
 
     groupTeamsList.append(team1Name,team2Name,team3Name,team4Name)
+    round1.append(round1Div)
+    round2.append(round2Div)
+    round3.append(round3Div)
 }
